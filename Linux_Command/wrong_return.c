d7d8afdc820ac5295dc50f1c89c76c2dfd4f63e5
he1l0world <chen@localhost.localdomain>
2017-08-02 16:53:28 +0800

    setjmp和longjmp函数

---
+++ Linux_Command/wrong_return.c
@@ -0,0 +1,50 @@
+/*************************************************************************
+	> File Name: wrong_return.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年08月02日 星期三 16时40分46秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<setjmp.h>
+#include<signal.h>
+
+jmp_buf env; //保存待跳转位置的栈信息
+
+/*信号SIGRTMIN+15的处理函数*/
+void handler_sigrtmin15(int  signo)
+{
+    printf("recv SGRTMIN+15\n");
+    longjmp(env,1);//返回到env处，注意第二个参数的值
+}
+
+/*信号SIGRTMAX-15的处理函数*/
+void handler_sigrtmax15(int signo)
+{
+    printf("recv SGRTMAX-15\n");
+    longjmp(env , 2);//返回到env处，注意第二个参数的值
+}
+
+int main ()
+{
+    /*设置返回点*/
+    switch (setjmp(env))
+    {
+        case 0:
+            break;
+        case 1:
+            printf("return from SIGRTMIN+15\n");
+            break;
+        case 2:
+            printf("return from SIGRTMAN_15\n");
+            break;
+        default:
+            break;
+    }
+
+    /*捕捉信号，安装信号处理函数*/
+    signal(SIGRTMIN+15,handler_sigrtmin15);
+    signal(SIGRTMAX-15 ,handler_sigrtmax15);
+    while(1);
+    return 0;
+}
