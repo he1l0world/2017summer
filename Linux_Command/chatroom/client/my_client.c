23dae3813f3c8fbab97f89aba118bf35140271db
chen <hellochen19970916@gmail.com>
2017-08-10 14:35:08 +0800

    初始版

--- Linux_Command/chatroom/client/my_client.c
+++ Linux_Command/chatroom/client/my_client.c
@@ -11,22 +11,64 @@
 #include<arpa/inet.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<fcntl.h>
+#include<sys/stat.h>
 #include<unistd.h>
+#include<unistd.h>
+#include<libgen.h>
 #include<string.h>
 #include<pthread.h>
-#include"client_base.h"
-#include"base.h"
+#include<readline/history.h>
+#include<readline/readline.h>
+#define BUFSIZE 128
+#define SIGN_IN 0
+#define SIGN_UP 1
+#define UPDATE_FILE 2
+#define DOWNLOAD_FILE 3
+#define CHAT_ADD 4
+#define CHAT_PRI 5
+#define CHAT_CREATE 6
+#define CHAT_JOIN 7
+#define INVALID_USERINFO  'n'
+#define VALID_USERINFO  'y'
 
+typedef struct user
+{
+    char username[32];
+    char password[32];
+}USER;
+struct node
+{
+    struct user  users;
+    char buf[BUFSIZE];
+    int type;
+    char filename[BUFSIZE];
+    char send[BUFSIZE];
+    char recv[BUFSIZE];
+    int id;
+};
+void update_file();
+void download_file();
+void chat_com();
+void chat_pri();
+void my_err(const char *err_string ,int line);
+void get_input(char *buf);
+void sign_in(int conn_fd ,struct node Buf ,struct sockaddr_in serv_addr);
+void sign_up(int conn_fd  ,struct node Buf,struct sockaddr_in serv_addr);
+int input_userinfo(int conn_fd ,struct node Buf);
+void main_UI();
+
+
+/**********************************************************************/
+char *port = "4507";
+char *adr = "127.0.0.1";
+struct sockaddr_in serv_addr;
+struct node *Buf;
+int conn_fd ;
 int main ()
 {
-    char *port = "4507";
-    char *adr = "127.0.0.1";
     int serv_port = atoi(port);
-    struct sockaddr_in serv_addr;
-    int conn_fd ;
-    USER *userinfo;
-    memset(userinfo, 0 ,sizeof(USER));
-
+    Buf = (struct node*)malloc(sizeof(struct node));
     conn_fd = socket(AF_INET , SOCK_STREAM , 0);//创建套接字
     memset(&serv_addr , 0 ,sizeof(struct sockaddr_in));
     serv_addr.sin_family = AF_INET;
@@ -35,7 +77,6 @@ int main ()
     if(conn_fd < 0)
         my_err("socket" , __LINE__);
     char ch;
-    char choice[32];
     while(1)
     {
         do
@@ -49,22 +90,19 @@ int main ()
             printf("-----------\n");
             printf(" 请选择:  ");
             ch = getchar();
-        }while(ch== 'q' || ch == 'Q');
-        if(ch == 'q' || ch== 'Q') 
+        }while(ch != 'q'&& ch!='a'&&ch!='b');
+        if(ch == 'q' || ch == 'Q') 
             break;
         switch(ch)
         {
             case 'a':
             case 'A':
-                sign_in(conn_fd,userinfo,serv_addr);
-                strcpy(choice,"sign_in");
-                send(conn_fd,choice,strlen(choice), 0);
+                printf("helloworld\n");
+                sign_in(conn_fd,*Buf,serv_addr);
                 break;
             case 'b':
             case 'B':
-                sign_up(conn_fd ,userinfo,serv_addr);
-                strcpy(choice,"sign_up");
-                send(conn_fd,choice,strlen(choice), 0);
+                sign_up(conn_fd ,*Buf,serv_addr);
                 break;
             default:
                 break;
@@ -73,5 +111,303 @@ int main ()
     close(conn_fd);
     return 0;
 }
+
+
+/********************************************************************/
+void update_file()
+{
+    struct node *post_buf , *recv_buf;
+    int fd , j;
+    char ch;
+    char *filename , *name;
+    char content[128];
+    post_buf = (struct node *)malloc(sizeof(struct node));
+    recv_buf = (struct node *)malloc (sizeof(struct node));
+    do
+    {
+        system("clear");
+        fflush(stdin);
+        j = 0;
+        memset(post_buf , 0 ,sizeof(struct node));
+        memset(content, 0 ,sizeof(content));
+       /* printf("请输入文件路径:(绝对路径)    ");
+        scanf("%s",filename);*/
+        filename = readline("请输入文件路径:   ");
+        printf("filename %s\n",filename);
+        post_buf->type = UPDATE_FILE;
+        for(int i = 0 ; filename[i] !='\0'  ; i++)
+        {
+            if(filename[i] == ' ')
+                continue;
+            post_buf->filename[j] = filename[i];
+            j++;
+            if(filename[i] == '/')
+            {
+                j = 0;
+            }
+        }
+        post_buf->filename[j] = '\0';
+        if(send(conn_fd ,post_buf ,sizeof(struct node) , 0) <0 )
+            my_err("send",__LINE__);
+
+        printf("filename = %s\n",filename);
+        if((fd = open(filename,O_RDONLY)) < 0)
+            my_err("open" , __LINE__);
+        
+        while(read(fd,content,sizeof(content)) > 0)
+        {
+            if(send(conn_fd , content , sizeof(content) , 0) < 0)
+                my_err("send" , __LINE__);
+        }
+        close(fd);
+        memset(content , 0 ,128);
+        char *end = "#end";
+        send(conn_fd , end , sizeof(end) , 0);
+        if(recv(conn_fd , recv_buf , sizeof(struct node) , 0) < 0)
+            my_err("recv",__LINE__);
+        if(strcmp(recv_buf->buf , "success!") == 0)
+            printf("上传成功!\n");
+        else
+            printf("上传失败!\n");
+        printf("是否继续上传?(y/n)   ");
+        ch = getchar();
+        ch = getchar();
+    }while(ch != 'n');
+}
+void download_file()
+{
+    int fd;
+    char ch;
+    int ret;
+    char content[128];
+    struct node *recv_buf;
+    recv_buf = (struct node*)malloc(sizeof(struct node));
+    do
+    {
+        system("clear");
+        fflush(stdin);
+        memset(recv_buf , 0 ,sizeof(struct node));
+        memset(content ,0 , 128);
+        fflush(stdin);
+        system("clear");
+        printf("请输入想要下载文件ID:  ");
+        scanf("%d",recv_buf->id);
+        recv_buf->type = DOWNLOAD_FILE;
+        /*----------------不全-------------*/
+        if((ret =recv(conn_fd , recv_buf ,sizeof(struct node) , 0)) < 0)
+            my_err("recv",__LINE__);
+        if(fd = open(recv_buf->filename , O_CREAT|O_WRONLY|O_TRUNC,0644) <0)
+            my_err("open",__LINE__);
+        recv(conn_fd , content , sizeof(content),0);
+        while(strcmp(content,"#end") != 0)
+        {
+            if(strcmp(content , "#end") != 0)
+            {
+                write(fd ,content , sizeof(content));
+                memset(content , 0 ,sizeof(content));
+            }
+            if(recv(conn_fd , content ,sizeof(content), 0) < 0)
+                my_err("recv", __LINE__);
+        }
+        printf("是否继续下载?(y/n)");
+        ch = getchar();
+    }while(ch != 'n');
+}
+void chat_pri()
+{
+    char ch;
+    struct node *post_buf;
+    post_buf = (struct node*)malloc(sizeof(struct node));
+    while(1)
+    {
+        do
+        {
+            system("clear");
+            printf("---------------------------\n");
+            printf("       a加好友\n");
+            printf("       b选择好友聊天\n");
+            printf("       q退出  \n");
+            printf("---------------------------\n");
+            printf("请选择:   ");
+            ch = getchar();
+        }while(ch!='a' && ch != 'b' && ch != 'q');
+        if(ch == 'q')
+            break;
+        if(ch == 'a')
+        {
+            post_buf->type = CHAT_ADD;
+        }
+        else
+        {
+            post_buf->type = CHAT_PRI;
+        }
+    }
+}
+void chat_com()
+{
+    char ch;
+    struct node *post_buf;
+    post_buf = (struct node*)malloc(sizeof(struct node));
+    while(1)
+    {
+        do
+        {
+            printf("---------------------------\n");
+            printf("       a创建群聊\n");
+            printf("       b加入群聊\n");
+            printf("       q退出\n");
+            printf("---------------------------\n");
+            printf("请选择: \n");
+            ch = getchar();
+        }while(ch != 'a' && ch != 'b' && ch != 'q');
+        if(ch == 'q')
+            break;
+        if(ch == 'a')
+        {
+            post_buf->type = CHAT_CREATE;
+        }
+        else
+        {
+            post_buf->type = CHAT_JOIN;
+        }
+    }
+}
+void my_err(const char *err_string ,int line)
+{
+    fprintf(stderr , "line :%d",line);
+    perror(err_string);
+    exit(1);
+}
+
+
+void sign_up(int conn_fd , struct node Buf , struct sockaddr_in serv_addr)
+{
+    while(1)
+    {
+        memset(&Buf,0,sizeof(struct node));
+        system("clear");
+        printf("请输入帐号:  ");
+        scanf("%s",Buf.users.username);
+        printf("请输入密码:  ");
+        scanf("%s",Buf.users.password);
+        Buf.type = 1;
+        if(connect(conn_fd ,(struct sockaddr*)&serv_addr , sizeof(struct sockaddr_in) ) < 0 )
+            my_err("connect" , __LINE__);
+        if(send(conn_fd , &Buf,sizeof(Buf) , 0) < 0)
+            my_err("send", __LINE__);
+        if(recv(conn_fd ,&Buf , sizeof(struct node),0 ) < 0 )
+            my_err("recv",__LINE__);
+        if(Buf.buf[0] == 'y')
+        {
+            printf("注册成功\n");
+            printf("输入任意键继续\n");
+            getchar();
+            break;
+        }
+        else
+        {
+            printf("注册失败\n");
+            printf("输入任意键继续\n");
+            getchar();
+        }
+    }
+    if(Buf.buf[0] == 'y')
+        main_UI();
+}
+
+
+int input_userinfo(int conn_fd , struct node Buf)
+{
+    struct  node recv_buf;
+    char flag_userinfo;
+    int len;
+    if(send(conn_fd , &Buf , sizeof(struct node) , 0) < 0)
+        my_err("send",__LINE__);
+    if((len = recv(conn_fd ,&recv_buf ,sizeof(struct node), 0)) < 0)
+    {
+        printf("data is too long\n");
+        exit(1);
+    }
+    if(recv_buf.buf[0] == VALID_USERINFO)
+        return 1;
+    return 0;
+        
+}
+
+void main_UI ()
+{
+    char ch = 'a';
+    while(1)
+    {
+        do
+        {
+            fflush(stdin);
+            system("clear");
+            printf("-------------------------------------------\n");
+            printf("            a:  上传文件\n");
+            printf("            b:  下载文件\n");
+            printf("            c:  群聊\n");
+            printf("            d:  私聊\n");
+            printf("            q:  退出\n");
+            printf("-------------------------------------------\n");
+            printf("请输入你的选择:   ");
+            ch = getchar();
+        }while(ch != 'q' && ch !='a' && ch!='b'&&ch!='c'&&ch!='d');
+        if(ch == 'q' )
+            break;
+        switch(ch)
+        {
+            case 'a':
+            case 'A':
+                update_file();
+            break;
+            case 'b':
+            case 'B':
+                download_file();
+                break;
+            case 'c':
+            case 'C':
+                chat_pri();
+                break;
+            case 'd':
+            case 'D':
+                chat_com();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void sign_in(int conn_fd ,struct node Buf , struct sockaddr_in serv_addr)
+{
+    int ret,i;
+    int flag = 1;
+    do
+    {
+        memset(&Buf , 0 ,sizeof(struct node));
+        flag = 1;
+        system("clear");
+        printf("请输入帐号:  ");
+        scanf("%s",Buf.users.username);
+        printf("请输入密码:  ");
+        scanf("%s",Buf.users.password);
+        printf("conn_fd : %d addr %s\n",conn_fd , inet_ntoa(serv_addr.sin_addr));
+        if(connect(conn_fd , (struct sockaddr*)&serv_addr , sizeof(struct sockaddr_in)) < 0)
+            my_err("connect" , __LINE__);
+        Buf.type = SIGN_IN;
+        if(input_userinfo(conn_fd ,Buf ) == 0)
+            flag = 0;
+        if(flag == 0)
+        {
+            printf("帐号或者密码错误请重新输入！\n");
+            printf("请输入任意键继续！\n");
+            getchar();
+        } 
+    }while(flag == 0);
+    main_UI();
+    return ;
+}
+
 
 
--- Linux_Command/chatroom/server/my_server.c
+++ Linux_Command/chatroom/server/my_server.c
@@ -14,34 +14,79 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<pthread.h>
+#include<signal.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 #include<errno.h>
-#include"server_base.h"
 #define LISTENQ 20
 #define INVALID_USERINFO 'n'
 #define VALID_USERINFO 'y'
+#define BUFSIZE 128
+#define SIGN_IN 0
+#define SIGN_UP 1
+#define UPDATE_FILE 2
+#define DOWNLOAD_FILE 3
+#define CHAT_ADD 4
+#define CHAT_PRI 5
+#define CHAT_CREATE 6
+#define CHAT_JOIN 7
+struct user
+{
+    char username[32];
+    char password[32];
+};
+struct node
+{
+    struct user users;
+    char buf[BUFSIZE];
+    int type;
+    char filename[BUFSIZE];
+    char send[BUFSIZE];
+    char recv[BUFSIZE];
+    int id;
+};
+struct con_fd
+{
+    int stat;
+    int fd;
+    char name[32];
+};
+int check_account(struct node Buf);
+void my_err(const char * err_string ,int line);
+void sign_handle(int signo);
+void send_data(int conn_fd ,struct node Buf );
+void *do_something(void *arg);
+void update_file(int con_fd ,struct node Buf);
+void download_file(int con_fd , struct node Buf);
 
+/*********************************************************************/
+int sock_fd ;
+int optval;
+int ret;
+struct con_fd conn_fd[1000];
+pthread_t *thread;
+socklen_t cli_len;
+int i = 0;
+struct sockaddr_in serv_addr , cli_addr;
 
 int main ()
 {
-   int sock_fd ,conn_fd;
-    int optval;
-    int ret;
-    pthread_t *thread;
-    socklen_t cli_len;
-    struct sockaddr_in serv_addr;
-    char recv_buf[128];
+    int conn_fd1;
+    char *addr = "127.0.0.1";
+    //初始化服务端地址结构
+    memset(&serv_addr , 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(4507);
+    //serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    inet_aton(addr , &serv_addr.sin_addr);
+
     sock_fd = socket(AF_INET , SOCK_STREAM , 0);//创建套接字
     if(sock_fd < 0 )
         my_err("socket" ,__LINE__);
     optval = 1;
     if(setsockopt(sock_fd , SOL_SOCKET,SO_REUSEADDR,(void*)&optval ,sizeof(int)) < 0)//设置套接字可复用
-        my_err("setsockopt" ,__LINE__);
-
-    //初始化服务端地址结构
-    memset(&serv_addr , 0, sizeof(struct sockaddr_in));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(4507);
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+            my_err("setsockopt" ,__LINE__);
+    
     if(bind(sock_fd , (struct sockaddr*)&serv_addr,sizeof(struct sockaddr_in)) < 0)//绑定套接字到地址
         my_err("bind",__LINE__);
     
@@ -54,13 +99,129 @@ int main ()
     thread = (pthread_t*)malloc(sizeof(pthread_t)*65535);
     while(1)
     {
-        int i = 0;
-        conn_fd = accept(sock_fd , (struct sockaddr*)&serv_addr ,&cli_len);
-        if(conn_fd < 0)
-            my_err("accept" ,__LINE__);
+        //signal(SIGINT , sign_handle);
+        conn_fd1 = accept(sock_fd , (struct sockaddr*)&cli_addr ,&cli_len );
+        if(conn_fd1 < 0)
+            my_err("accept" , __LINE__);
+    
+        conn_fd[i].fd = conn_fd1;
+        printf("conn_fd %d addr %s\n",conn_fd1 ,inet_ntoa(serv_addr.sin_addr));
         pthread_create(&thread[i],NULL,do_something,NULL);//创建子线程处理刚刚接收的请求
-        pthread_join(thread[i],NULL);
-        i++;
+        //conn_fd1 = 0;
+        //pthread_join(thread[i],NULL);
     }
-    return 0;
 }
+
+/*********************************************************************/
+
+
+void sign_handle(int signo)
+{
+
+}
+void my_err(const char *err_string ,int line)
+{
+    fprintf(stderr , "line %d ",line);
+    perror(err_string);
+    exit(1);
+}
+
+int check_account(struct node Buf)
+{
+    int i;
+    if(Buf.users.username == NULL || Buf.users.password == NULL)
+    {
+        printf("in find_name, NULL pointer");
+        return -2;
+    }
+    char input_buf[32];
+    /*连接myqsl查找用户名密码 */
+}
+void update_file(int con_fd ,struct node Buf)
+{
+    struct node *post_buf;
+    post_buf = (struct node*)malloc(sizeof(struct node));
+    int fd;
+    char content[128];
+    fd = open(Buf.filename ,O_CREAT|O_WRONLY|O_TRUNC,0644);
+    recv(con_fd , content ,sizeof(content), 0);
+    while(strcmp(content,"#end") != 0)
+    {
+        if(strcmp(content,"#end") != 0)
+        {
+            write(fd , content,sizeof(content));
+        }
+        if(recv(con_fd ,content,sizeof(content), 0) < 0)
+            my_err("recv",__LINE__);
+    }
+    close(fd);
+    strcpy(post_buf->buf , "success!");
+    if(send(con_fd , post_buf , sizeof(struct node), 0) < 0)
+        my_err("send",__LINE__);
+}
+void download_file(int con_fd , struct node Buf)
+{
+    ;
+}
+void *do_something(void* arg)
+{   
+    int con_fd =conn_fd[i].fd ;
+    int t = i;
+    i++;
+    int ret, how;
+    struct node *Buf ,*recv_buf;
+    Buf = (struct node *)malloc(sizeof(struct node));
+    recv_buf = (struct node*)malloc(sizeof(struct node));
+    printf("accept a new client ,ip %s\n",inet_ntoa(cli_addr.sin_addr));
+    while(1)
+    {
+        how = -1;
+        if((ret = recv(con_fd ,  recv_buf , sizeof(struct node) , 0)) < 0)
+        {
+            my_err("recv" ,__LINE__);
+        }
+        how = recv_buf->type;
+        switch(how)
+        {
+            case 0:
+                //sign_in
+                
+                conn_fd[t].stat = 1;
+                strcpy(conn_fd[t].name,recv_buf->users.username);
+                strcpy(Buf->buf,"y");
+                send(con_fd ,Buf,sizeof(struct node),0 );
+                printf("%s %s\n",recv_buf->users.username,recv_buf->users.password);
+                break;
+            case 1:
+                //sign_up
+                conn_fd[t].stat = 1;
+                strcpy(conn_fd[t].name,recv_buf->users.username);
+                strcpy(Buf->buf,"y");
+                send(con_fd , Buf,sizeof(struct node) , 0);
+                break; 
+            case 2:
+                //update_file
+                update_file(con_fd ,*recv_buf);
+                break;
+            case 3:
+                //download_file
+                download_file(con_fd ,*recv_buf);
+                break;
+            case 4:
+                //chat_add
+                break;
+            case 5:
+                //chat_pri
+                break;
+            case 6:
+                //chat_create
+                break;
+            case 7:
+                //chat_join
+                break;
+            default:
+                break;;
+        }
+    }
+}
+
